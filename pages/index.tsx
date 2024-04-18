932d652bf8e631c5aebc19120d462bb9287ecf83
Lukas Bombach <lukas.bombach@googlemail.com>
2024-04-18 16:03:51 +0200

    all good

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
-import { Nested } from "../components/Nested";
+import dynamic from "next/dynamic";
+
+const DynamicTest = dynamic(() => import("../components/Test").then((mod) => mod.DynamicTest), {
+  loading: () => <div>Loading</div>,
+});
 
 export default function Page() {
   return (
     <main>
       <h1>Hello, Next.js!</h1>
-      <Nested />
+      <DynamicTest />
     </main>
   );
 }
